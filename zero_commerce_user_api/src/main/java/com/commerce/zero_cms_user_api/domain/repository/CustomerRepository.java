3a4c007dc26ebd016ecb33e8c889a647140b0f51
hakdang113 <zapper15@naver.com>
2023-11-16 15:47:16 +0900

    feat : [003-Email-Verification] 고객 회원 가입 시 이메일 인증 및 이메일 인증 예외 처리 구현
    - 고객 회원 가입 테이블 생성
    - 고객 회원 가입 서비스 구현
    - 고객 회원 가입 시 인증 예외 처리
    
    - 수정 중
    - Mailgun 이용 시 401 Unauthorized 에러 발생
    - 에러 지속 발생 시 JavaMailSender를 이용한 방식으로 수정 예정

--- zero_commerce_user_api/src/main/java/com/commerce/zero_cms_user_api/domain/repository/CustomerRepository.java
+++ zero_commerce_user_api/src/main/java/com/commerce/zero_cms_user_api/domain/repository/CustomerRepository.java
@@ -4,10 +4,13 @@ import com.commerce.zero_cms_user_api.domain.model.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 // Entity 모델들을 통해 데이터를 주고 받기 위한 Repository 인터페이스
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
+    Optional<CustomerEntity> findByEmail(String email);
 
 }
