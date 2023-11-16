e2d3949aa13b8a3a4364dd8c849e519245233140
hakdang113 <zapper15@naver.com>
2023-11-16 14:37:02 +0900

    feat : [000] `README.md` 작성
    - 수정중

---
+++ src/main/java/com/commerce/zero_commerce_prj/ZeroCommercePrjApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.zero_commerce_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroCommercePrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroCommercePrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerce/zero_commerce_prj/ZeroCommercePrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.zero_commerce_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroCommercePrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
